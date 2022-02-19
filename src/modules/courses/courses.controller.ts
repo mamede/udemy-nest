c0d15f7cb517c983974a28dc4143182475369c75
Mamede <felipebrasilmv@gmail.com>
2022-02-19 14:47:12 -0300

    feat: criando crud

--- src/modules/courses/courses.controller.ts
+++ src/modules/courses/courses.controller.ts
@@ -1,14 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de cursos');
   }
 
   @Get(':id')
-  findOne(@Param() params) {
-    return `Curso #${params.id}`;
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualizacao do Curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Exclusao do Curso #${id}`;
   }
 }
