47c3e5ebac88d367eb17544f37211dba8fabccfd
Mamede <felipebrasilmv@gmail.com>
2022-02-19 14:29:34 -0300

    first commit

---
+++ src/modules/courses/courses.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll() {
+    return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `Curso #${params.id}`;
+  }
+}
