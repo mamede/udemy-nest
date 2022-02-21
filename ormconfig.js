ecb79c113b30b349911cbf1a1c9202ab13591e77
mamede <felipebrasilmv@gmail.com>
2022-02-21 00:32:15 -0300

    feat: rodando o app com docker-compose

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
   username: 'postgres',
-  password: 'nestjs',
-  database: 'postgres',
+  password: 'docker',
+  database: 'cursonestjs',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   cli: {
